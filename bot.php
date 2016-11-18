bd3b396f4aca58a8e6ae0d0c80a72c37a35f41a7
bank <titipong_bb@hotmail.com>
2016-11-18 14:16:51 +0700

    edit

--- bot.php
+++ bot.php
@@ -5,44 +5,73 @@ $access_token = '6t9nTGUM4MSBtfVGcj3hTogXFEr7/Uc1ZrSnJJpuzHsegwHbA0Ja0hbePr2xBz8
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+
+$jsonObj = json_decode($json_string);/
+         $to = $jsonObj->{"result"}[0]->{"content"}->{"from"}; 
+         $text = $jsonObj->{"result"}[0]->{"content"}->{"text"}; 
+         $text_ex = explode(':', $text);
+
+
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            $text = $event['message']['text'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+        if($text_ex[0] == "อยากรู้"){  
+         $ch1 = curl_init(); 
+         curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false); 
+         curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true); 
+         curl_setopt($ch1, CURLOPT_URL, 'https://th.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$text_ex[1]); 
+         $result1 = curl_exec($ch1); 
+         curl_close($ch1); $obj = json_decode($result1, true);
+          foreach($obj['query']['pages'] as $key => $val)
+            {
+                $result_text = $val['extract'];
+            } 
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+          if(empty($result_text)){//ถ้าไม่พบให้หาจาก en
+           $ch1 = curl_init(); 
+            curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false); 
+            curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true); 
+            curl_setopt($ch1, CURLOPT_URL, 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$text_ex[1]); 
+            $result1 = curl_exec($ch1); curl_close($ch1); 
+            $obj = json_decode($result1, true); 
+            foreach($obj['query']['pages'] as $key => $val){ $result_text = $val['extract']; } } 
+            if(empty($result_text)){//หาจาก en ไม่พบก็บอกว่า ไม่พบข้อมูล ตอบกลับไป 
+                $result_text = 'ไม่พบข้อมูล'; } 
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
 
-			echo $result . "\r\n";
-		}
-	}
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
+        }
+    }
 }
-
 echo "OK";
