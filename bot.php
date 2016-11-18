c7504fee408c07e6878d5af15968248ebe646fa9
bank <titipong_bb@hotmail.com>
2016-11-18 14:41:36 +0700

    a

--- bot.php
+++ bot.php
@@ -50,14 +50,14 @@ if (!is_null($events['events'])) {
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => $text
+                'text' => $result_text
             ];
 
             // Make a POST Request to Messaging API to reply to sender
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
-                'messages' => [$result_text],
+                'messages' => [$messages],
             ];
             $post = json_encode($data);
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
