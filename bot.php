6b4f8486a1fc1fc8ec03229baabea51da827edc1
bank <titipong_bb@hotmail.com>
2016-11-21 10:08:26 +0700

    ฟ

--- bot.php
+++ bot.php
@@ -36,14 +36,14 @@ if (!is_null($events['events'])) {
 
 
             // Get text sent
-            $text = $event['message']['result_text'];
+             $result_text = $event['message']['text'];
             // Get replyToken
             $replyToken = $event['replyToken'];
 
             // Build message to reply back
             $messages = [
                 'type' => 'text',
-                'text' => $text
+                'text' => $result_text
             ];
 
             // Make a POST Request to Messaging API to reply to sender
