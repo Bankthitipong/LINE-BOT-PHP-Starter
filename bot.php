d831c859bcfa29666dbb7ea714be3cb265e77047
bank <titipong_bb@hotmail.com>
2016-11-21 08:50:30 +0700

    a

--- bot.php
+++ bot.php
@@ -5,71 +5,54 @@ $access_token = '6t9nTGUM4MSBtfVGcj3hTogXFEr7/Uc1ZrSnJJpuzHsegwHbA0Ja0hbePr2xBz8
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-
-
-
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
     // Loop through each event
     foreach ($events['events'] as $event) {
         // Reply only when message sent is in 'text' format
         if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-            // Get text sent
-            $text = $event['message']['text'];
-            // Get replyToken
-            $replyToken = $event['replyToken'];
 
-            // Build message to reply back
-            // $messages = [
-            //     'type' => 'text',
-            //     'text' => $result_text
-            // ];
-            
-// $to = $events->{"result"}[0]->{"content"}->{"from"}; //หาผู้ส่ง 
-// $text = $events->{"result"}[0]->{"content"}->{"text"}; //หาข้อความที่โพสมา 
-$text_ex = explode(':', $text); 
 
- if ($text_ex[0] == "wiki"){ 
-     $ch1 = curl_init(); 
+
+        $text_ex = explode(':', $text); //เอาข้อความมาแยก : ได้เป็น Array 
+         if($text_ex[0] == "อยากรู้"){ //ถ้าข้อความคือ "อยากรู้" ให้ทำการดึงข้อมูลจาก Wikipedia หาจากไทยก่อน 
+         $ch1 = curl_init(); 
          curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false); 
          curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true); 
          curl_setopt($ch1, CURLOPT_URL, 'https://th.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$text_ex[1]); 
-
          $result1 = curl_exec($ch1); 
-         curl_close($ch1); 
-         $obj = json_decode($result1, true);
-
-          foreach($obj['query']['pages'] as $key => $val)
-          {
-           $result_text = $val['extract']; 
-          } 
-
+         curl_close($ch1); $obj = json_decode($result1, true);
+          foreach($obj['query']['pages'] as $key => $val){
+           $result_text = $val['extract']; } 
 
           if(empty($result_text)){//ถ้าไม่พบให้หาจาก en
            $ch1 = curl_init(); 
             curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false); 
             curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true); 
             curl_setopt($ch1, CURLOPT_URL, 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='.$text_ex[1]); 
-
-            $result1 = curl_exec($ch1); 
-            curl_close($ch1); 
+            $result1 = curl_exec($ch1); curl_close($ch1); 
             $obj = json_decode($result1, true); 
-
-            foreach($obj['query']['pages'] as $key => $val)
-                { 
-                    $result_text = $val['extract']; 
-                }
-                 } 
-
+            foreach($obj['query']['pages'] as $key => $val){ $result_text = $val['extract']; } } 
             if(empty($result_text)){//หาจาก en ไม่พบก็บอกว่า ไม่พบข้อมูล ตอบกลับไป 
                 $result_text = 'ไม่พบข้อมูล'; } 
-            
+
+
+            // // Get text sent
+            // $text = $event['message']['text'];
+            // // Get replyToken
+            // $replyToken = $event['replyToken'];
+
+            // // Build message to reply back
+            // $messages = [
+            //     'type' => 'text',
+            //     'text' => $text
+            // ];
 
             // Make a POST Request to Messaging API to reply to sender
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
-                'messages' => $messages,
+                'messages' => [$ $result_text],
             ];
             $post = json_encode($data);
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -84,8 +67,8 @@ $text_ex = explode(':', $text);
             curl_close($ch);
 
             echo $result . "\r\n";
+            }
         }
-    }
     }
 }
 echo "OK";
