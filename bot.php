e369a8b384f5825d18866bba5044cda9132a0bd2
bank <titipong_bb@hotmail.com>
2016-11-21 08:55:47 +0700

    a

--- bot.php
+++ bot.php
@@ -37,22 +37,22 @@ if (!is_null($events['events'])) {
                 $result_text = 'ไม่พบข้อมูล'; } 
 
 
-            // // Get text sent
-            // $text = $event['message']['text'];
-            // // Get replyToken
-            // $replyToken = $event['replyToken'];
+            // Get text sent
+            $text = $event['message']['text'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
 
-            // // Build message to reply back
-            // $messages = [
-            //     'type' => 'text',
-            //     'text' => $text
-            // ];
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $result_text
+            ];
 
             // Make a POST Request to Messaging API to reply to sender
             $url = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
-                'messages' => [$result_text],
+                'messages' => [$messages],
             ];
             $post = json_encode($data);
             $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
