4179ded88cea9527b5179c60ee0e552b6894bd27
bank <titipong_bb@hotmail.com>
2016-11-23 11:14:18 +0700

    1

--- bot1.php
+++ bot1.php
@@ -113,7 +113,7 @@ if (!is_null($events['events'])) {
             ];
 
             // Make a POST Request to Messaging API to reply to sender
-            $url  = 'https://api.line.me/v2/bot1/message/reply';
+            $url  = 'https://api.line.me/v2/bot/message/reply';
             $data = [
                 'replyToken' => $replyToken,
                 'messages'   => [$messages],
